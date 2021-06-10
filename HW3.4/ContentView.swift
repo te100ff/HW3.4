8a251de183caa4496e240343ffb44553b58e64b5
Stanislav <te100ff@inbox.ru>
2021-06-10 23:07:19 +0300

    Initial Commit

---
+++ HW3.4/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HW3.4
+//
+//  Created by Stanislav Testov on 10.06.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
